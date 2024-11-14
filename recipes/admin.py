16284efdd7d75e882713d92aaea5266886aa65ff
D <dylankfilm@gmail.com>
2024-11-14 12:09:08 +0900

    added recipe details

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Recipe
 
+
 # Register your models here.
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
-        'title',
-        'meal_type',
-        'calories',
-        'instructions',
-        'ingredients',
-        'image'
+        "title",
+        "meal_type",
+        "calories",
+        "instructions",
+        "ingredients",
+        "image",
     )
-list_filter = ('meal_type',)
+
+
+list_filter = ("meal_type",)
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -7,21 +7,31 @@ from .forms import RecipeForm
 
 
 class Recipes(ListView):
-    """ View all recipes """
-    template_name = 'recipes/recipes.html'
+    """View all recipes"""
+
+    template_name = "recipes/recipes.html"
     model = Recipe
-    context_object_name = 'recipes'
+    context_object_name = "recipes"
+
+
+class RecipeDetail(DetailView):
+    """View a single recipe"""
+
+    template_name = "recipes/recipe_detail.html"
+    model = Recipe
+    context_object_name = "recipe"
 
 
 class AddRecipe(LoginRequiredMixin, CreateView):
-    """ 
+    """
     Add Recipe View
     """
-    template_name = 'recipes/add_recipe.html'
+
+    template_name = "recipes/add_recipe.html"
     model = Recipe
     form_class = RecipeForm
-    success_url = '/recipes/'
-    
+    success_url = "/recipes/"
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
