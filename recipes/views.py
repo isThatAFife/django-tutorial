5809d4060b2712d599772ab54069dc3d98f2e7b3
D <dylankfilm@gmail.com>
2024-11-18 14:48:14 +0900

    added recipe delete

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,13 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import (
+    CreateView, 
+    ListView, 
+    DetailView, 
+    DeleteView
+)
+
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin, LoginRequiredMixin
+)
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -35,3 +44,12 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
+
+
+class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Delete a recipe"""
+    model = Recipe
+    success_url = "/recipes/"
+    
+    def test_func(self):
+        return self.request.user == self.get_object().user
